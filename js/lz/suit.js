16f67977d85b8a2b59afb51237099d87612f1fae
zhen920907 <1192695660@qq.com>
2018-08-30 23:14:49 +0800

    修改套装图鉴主图，合并公共头尾

--- js/lz/suit.js
+++ js/lz/suit.js
@@ -1,17 +1,31 @@
 
+
 //给图片按钮注册事件
 $(function () {
+
     $('.btn-ico >li').click(function () {
         $(this).addClass("now").siblings().removeClass("now");
 
-
         //当前li的下标
         var idx = $(this).index();
-        console.log($(this).children().attr('title'));
-        $('.section .tag_title .content_title').text($(this).children().attr('title'));
+        $('.section .tag_title .content_title').text($(this).find('a').attr('title'));
 
         $(".section .info > ul").eq(idx).show().siblings().hide();
     });
+
+    //给下面所有图片注册鼠标经过事件
+    var $lis = $(".section .info > ul > li");
+
+     $lis.mouseenter(function () {
+         $(this).css('opacity', 1).siblings().css('opacity',.5);
+
+     });
+
+    $(".section .info > ul").mouseleave(function () {
+        $(this).children().css('opacity',1);
+
+    });
+
 
 });
 
