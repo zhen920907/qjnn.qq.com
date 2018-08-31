842c576d879c0d10e1494a0337180655b48589e1
gaoyuany <861099134@qq.com>
2018-08-31 21:42:02 +0800

    add community files

--- js/community/community.js
+++ js/community/community.js
@@ -1,31 +1,37 @@
-$(function() {
-  $('#tabs').carouFredSel({
-    circular: false,
-    items: 1,
-    width: '100%',
-    auto: false,
-    pagination: {
-      container: '#pager',
-      anchorBuilder: function( nr ) {
-        return '<a href="#">' + $(this).find('h3').text() + '</a>';
-      }
-    }
-  });
-  $('#tabs').mouseleave(function(){
-    return null;
-  })
-});
+// $(function() {
+//   $('#tabs').carouFredSel({
+//     circular: false,
+//     items: 1,
+//     width: '100%',
+//     auto: false,
+//     pagination: {
+//       container: '#pager',
+//       anchorBuilder: function( nr ) {
+//         return '<a href="#">' + $(this).find('h3').text() + '</a>';
+//       }
+//     }
+//   });
+//   $('#tabs').mouseleave(function(){
+//     return null;
+//   });
+// });
 
 
 // tab Start
 
-$('#myTabs a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-});
+// $('#myTabs a').click(function (e) {
+//   e.preventDefault()
+//   $(this).tab('show')
+// });
 
-$('#myTabs a[href="#profile"]').tab('show') // Select tab by name
-$('#myTabs a:first').tab('show') // Select first tab
-$('#myTabs a:last').tab('show') // Select last tab
-$('#myTabs li:eq(2) a').tab('show') // Select third tab (0-indexed)
+// ??????
+// console.log($('#myTabs a[href="#profile"]'))
+// $('#myTabs a[href="#profile"]').tab('show') // Select tab by name
+// $('#myTabs a:first').tab('show') // Select first tab
+// $('#myTabs a:last').tab('show') // Select last tab
+// $('#myTabs li:eq(2) a').tab('show') // Select third tab (0-indexed)
+
+$(".solution-more-slide").slide({
+  effect: 'fold'
+});
 // tab End
